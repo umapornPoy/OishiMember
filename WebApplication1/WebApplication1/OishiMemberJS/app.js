52b05808d0aa992c25000afc59465af93e5368cf
umapornPoy <p.tunlang7@gmail.com>
2016-09-08 15:10:55 +0700

    create oishimember JS

---
+++ WebApplication1/WebApplication1/OishiMemberJS/app.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module('OishiMember', ['ngRoute', 'ui.bootstrap']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+
+      
+        .when("/Map", {
+            templateUrl: "Map/Map"
+        })
+        .otherwise({
+            templateUrl: "release/index"
+        })
+});
